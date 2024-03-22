c2e120693cf372ede9192785535a3850ab28159d
AM2101322 <114479943+AM2101322@users.noreply.github.com>
2024-03-22 21:08:35 +0200

    CSV works now

--- C_code/CSV.cpp
+++ C_code/CSV.cpp
@@ -4,37 +4,103 @@
 
 using namespace std;
 
-void create() {
+void create_file(std::string file_name) {
     // file pointer
     fstream fout;
 
-    // opens an existing csv file or creates a new file.
-    fout.open("reportcard.csv", ios::out | ios::app);
-
-    cout << "Enter the details of 5 students: roll name maths phy chem bio" << endl;
-
-    int i, roll, phy, chem, math, bio;
-    string name;
-
     // Read the input
-    for (i = 0; i < 5; i++) {
-        cin >> roll >> name >> math >> phy >> chem >> bio;
+    int i;
+    std::string password;
+    std::string website;
 
-        // Insert the data to file
-        fout << roll << ", "
-             << name << ", "
-             << math << ", "
-             << phy << ", "
-             << chem << ", "
-             << bio
-             << "\n";
-    }
+    // opens an existing csv file or creates a new file.
+    fout.open(file_name, ios::out | ios::app); // Use file_name variable here
+
+    cout << "what is the website you are saving the password for: " << endl;
+    std::getline(std::cin >> std::ws, website);
+    
+
+    
+   
+    cout << "what is the password: " << endl;
+    std::getline(std::cin >> std::ws, password);
+    
+    // Insert the data to file
+    fout << website << ","
+        << password
+         << "\n";
+    
 
     // Close the file
     fout.close();
 }
+void read_file(std::string file_name) 
+{ 
+
+	// File pointer 
+	fstream fin; 
+
+	// Open an existing file 
+	fin.open("file_name", ios::in); 
+
+	// Get the roll number 
+	// of which the data is required 
+	int rollnum, roll2, count = 0; 
+	cout << "Enter the roll number "
+		<< "of the student to display details: "; 
+	cin >> rollnum; 
+
+	// Read the Data from the file 
+	// as String Vector 
+	vector<string> row; 
+	string line, word, temp; 
+
+	while (fin >> temp) { 
+
+		row.clear(); 
+
+		// read an entire row and 
+		// store it in a string variable 'line' 
+		getline(fin, line); 
+
+		// used for breaking words 
+		stringstream s(line); 
+
+		// read every column data of a row and 
+		// store it in a string variable, 'word' 
+		while (getline(s, word, ', ')) { 
+
+			// add all the column data 
+			// of a row to a vector 
+			row.push_back(word); 
+		} 
+
+		// convert string to integer for comparision 
+		roll2 = stoi(row[0]); 
+
+		// Compare the roll number 
+		if (roll2 == rollnum) { 
+
+			// Print the found data 
+			count = 1; 
+			cout << "Details of Roll " << row[0] << " : \n"; 
+			cout << "Name: " << row[1] << "\n"; 
+			cout << "Maths: " << row[2] << "\n"; 
+			cout << "Physics: " << row[3] << "\n"; 
+			cout << "Chemistry: " << row[4] << "\n"; 
+			cout << "Biology: " << row[5] << "\n"; 
+			break; 
+		} 
+	} 
+	if (count == 0) 
+		cout << "Record not found\n"; 
+} 
+
 
 int main() {
-    create(); // Add semicolon after function call
-    return 0; // Return statement added
+    std::string file_name;
+    cout << "Enter the file name: " << endl;
+    std::getline(std::cin >> std::ws, file_name);
+    create(file_name);
+    return 0;
 }
