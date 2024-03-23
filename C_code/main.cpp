691b4d049d9c42ce6eb723995e2cc2a0816a4b27
AM2101322 <114479943+AM2101322@users.noreply.github.com>
2024-03-23 22:56:43 +0200

    Update main.cpp

--- C_code/main.cpp
+++ C_code/main.cpp
@@ -85,18 +85,3 @@ int main()
     }
     return 0;
 }
-
-
-
-//make users 
-//https://youtu.be/fTdkqOS5_ro?si=qTB7xvjQmR5Sj0gt 👆👇
-// give users passwords
-// save user passwords 
-// encrypt the password
-// (make yser that the location of the file is saved with the username and password) 
-// make sure the user can get the password back 
-// make it save what the password is for 
-// save the password on a file (encrypt)
-// the user selects what password they want
-//(use CSV for this 👆👇)
-// after the user selected the password is saved on the clip bord
